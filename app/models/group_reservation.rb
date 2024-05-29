8a819b09bc2a06a5fa341a940b022cda18d3d821
Sara Ikeda <sara.ikeda10@gmail.com>
2024-05-29 16:04:36 -0300

    Reserva para Grupo tem validações

--- app/models/group_reservation.rb
+++ app/models/group_reservation.rb
@@ -1,3 +1,4 @@
 class GroupReservation < ApplicationRecord
+  validates :name, :start_date, :end_date, presence: true
   belongs_to :inn
 end
--- spec/system/group_reservations/inn_owner_create_group_reservation_spec.rb
+++ spec/system/group_reservations/inn_owner_create_group_reservation_spec.rb
@@ -32,5 +32,24 @@ describe "Inn owner creates a group reservation" do
     expect(current_path).to eq inn_path(owner.inn)
     expect(page).to have_content 'Reserva criada com sucesso!'
   end
-  
+
+  it "unsuccessfully" do
+    owner = InnOwner.create!(email: 'sara@email.com', password: 'password',
+      first_name: 'Sara', last_name: 'Ikeda', document: '76283993710')
+    Inn.create!(name: 'Pousada da Sara', inn_owner: owner, registration_number: '30638898000199', description: 'Para amigos', 
+    address_attributes: { address: 'Rua X', number: '100', city: 'Belém', state: 'AM', postal_code: '69067-080', neighborhood: 'Centro'})
+    
+    login_as owner, scope: :inn_owner
+    visit root_path
+    click_on 'Gestão de Pousadas'
+    click_on 'Reservar para Grupo'
+    fill_in "Nome",	with: ""
+    fill_in "Data Inicial",	with: ""
+    fill_in "Data Final",	with: ""
+    click_on 'Reservar'
+
+    expect(page).to_not have_content 'Reserva criada com sucesso!'
+    expect(page).to have_content 'Reservar para Grupo'
+    expect(page).to have_field 'Nome'
+  end
 end
