e9c1340819a94e895e30513518fd85c288ee99e2
Sara Ikeda <sara.ikeda10@gmail.com>
2024-05-29 15:21:02 -0300

    Dono de Pousada tem botão para reservar grupo

--- spec/system/group_reservations/inn_owner_create_group_reservation_spec.rb
+++ spec/system/group_reservations/inn_owner_create_group_reservation_spec.rb
@@ -4,7 +4,8 @@ describe "Inn owner creates a group reservation" do
   it "on the inn management view" do
     owner = InnOwner.create!(email: 'sara@email.com', password: 'password',
       first_name: 'Sara', last_name: 'Ikeda', document: '76283993710')
-    Inn.create!(name: 'Pousada da Sara', inn_owner: owner, registration_number: '30638898000199')
+    Inn.create!(name: 'Pousada da Sara', inn_owner: owner, registration_number: '30638898000199', description: 'Para amigos', 
+    address_attributes: { address: 'Rua X', number: '100', city: 'Belém', state: 'AM', postal_code: '69067-080', neighborhood: 'Centro'})
     
     login_as owner, scope: :inn_owner
     visit root_path
@@ -12,4 +13,5 @@ describe "Inn owner creates a group reservation" do
 
     expect(page).to have_button "Reservar para Grupo"
   end
+  
 end
